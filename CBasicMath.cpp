782e7ac1c6133fe89be6b32f9049371dfb6e93ac
Sayeed Salam <mssalam62@yahoo.com.au>
2023-08-09 18:23:27 +1000

    First CPP Commit

---
+++ CBasicMath.cpp
@@ -0,0 +1,13 @@
+// C++ class to unit test:
+#include "CBasicMath.h"
+
+int CBasicMath::Addition(int x, int y)
+{
+   return (x + y);
+}
+
+int CBasicMath::Multiply(int x, int y)
+{
+   return (x * y);
+}
+
---
+++ CBasicMath.h
@@ -0,0 +1,6 @@
+class CBasicMath
+{
+public:
+   int Addition(int x, int y);
+   int Multiply(int x, int y);
+};
